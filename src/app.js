26d56994060b4ba4f2671895e3a3705999448d6a
liuwanyong2017 <2674706698@qq.com>
2020-01-06 15:00:41 +0800

    通过parcel初始化项目的脚手架，加入scss等依赖。并写button的单文件组件

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import Vue from "vue";
+
+import Button from "./button/button.vue";
+
+
+Vue.component("y-button", Button);
+
+new Vue({
+    el: "#app"
+});
+
---
+++ src/button/button.vue
@@ -0,0 +1,35 @@
+<template>
+    <button class="yv-button">
+        <slot></slot>
+    </button>
+</template>
+
+<script>
+    export default {};
+
+</script>
+
+<style lang="scss">
+    .yv-button {
+        font-size: var(--font-size);
+        border: 1px solid var(--border-color);
+        border-radius: var(--button-radius);
+        padding: 4px 1em;
+
+        &:active {
+            background-color: white;
+        }
+
+        &:hover {
+            background-color: var(--button-hover-bg-color);
+        }
+
+        &:focus {
+            outline: none;
+        }
+
+        cursor: pointer;
+        display: inline-flex;
+        transition: all .5s linear;
+    }
+</style>
