ec6f18cbb24599249646f97a57368eac932b160d
liuwanyong2017 <2674706698@qq.com>
2020-01-06 22:05:07 +0800

    add 子元素只能是y-button组件的警告

--- src/button/buttongroup.vue
+++ src/button/buttongroup.vue
@@ -7,31 +7,46 @@
 </template>
 
 
-<script >
+<script>
     export default {
-
-    }
+        mounted() {
+            // console.log(this.$children,this.$el);
+            for (let child of this.$el.children) {
+                // console.log(child);
+                const name = child.nodeName.toLowerCase();
+                console.log(name);
+                if (name.toLowerCase() !== "button") {
+                    console.warn(`y-btn-group组件的子元素只能是y-button组件，而不是${name}`);
+                }
+            }
+        }
+    };
 </script>
 
 <style lang="scss">
-    $radius :var(--button-radius);
-    .yv-btngroup{
+    $radius: var(--button-radius);
+    .yv-btngroup {
         display: inline-flex;
         vertical-align: middle;
-        >.yv-button{
+
+        > .yv-button {
             border-radius: 0;
-            &:first-child{
-                border-bottom-left-radius:$radius;
-                border-top-left-radius:$radius;
+
+            &:first-child {
+                border-bottom-left-radius: $radius;
+                border-top-left-radius: $radius;
             }
-            &:last-child{
+
+            &:last-child {
                 border-bottom-right-radius: $radius;
                 border-top-right-radius: $radius;
             }
-            &:not(first-child){
-                margin-left:-1px;
+
+            &:not(first-child) {
+                margin-left: -1px;
             }
-            &:hover{
+
+            &:hover {
                 z-index: 10;
             }
         }
