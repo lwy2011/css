b6bd7c0d2c8252f7dc5c35d541b0fd1d134a6c70
liuwanyong2017 <2674706698@qq.com>
2020-01-12 21:05:11 +0800

    实现页面只能存在一个toast。

--- src/toast/plugin.js
+++ src/toast/plugin.js
@@ -1,21 +1,26 @@
 import Toast from "./toast.vue";
-import Vue from "vue";
 
+let created;
+const init = (onlyone, msg, props, Vue) => {
+    if (onlyone && created) {
+        created.close();
+    }
+    const Constructor = Vue.extend(Toast);
+    const vm = new Constructor(
+        {
+            propsData: props
+        }
+    );
+    msg && (vm.$slots.default = [msg]);
+    vm.$mount();
+    document.body.appendChild(vm.$el);
+    return vm;
+};
 export default {
     install(vue) {
-        vue.prototype.$y_toast = (
-            props, msg
-        ) => {
-             console.log(2,props);
-            const Constructor = Vue.extend(Toast);
-            const vm = new Constructor(
-                {
-                    propsData: props
-                }
-            );
-            msg && (vm.$slots.default = [msg]);
-            vm.$mount();
-            document.body.appendChild(vm.$el);
+        vue.prototype.$y_toast = (props, msg, onlyone = true) => {
+            // console.log(2, props);
+            created = init(onlyone, msg, props, vue);
         };
     }
 };
