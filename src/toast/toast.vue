dffb4b5ddf34f66f375535a73e0fce658f8d553c
liuwanyong2017 <2674706698@qq.com>
2020-01-12 23:13:11 +0800

    修改动画，不能依靠translateX(-50%)，定死在动画里，万一居中不是通过translate做的呢？

--- src/toast/toast.vue
+++ src/toast/toast.vue
@@ -2,14 +2,18 @@
     <div class="yv-toast"
          :class="position && `position-${position}`"
     >
-        <slot></slot>
-        <div v-if="html" v-html="html"></div>
-        <div v-if="closeBtn" class="toast-close" @click="closeBtnClick">
-            <div class="line">
-                <div class="border"></div>
-                <div class="mask"></div>
+        <div class="wrapper"
+             :class="position && `position-${position}`"
+        >
+            <slot></slot>
+            <div v-if="html" v-html="html"></div>
+            <div v-if="closeBtn" class="toast-close" @click="closeBtnClick">
+                <div class="line">
+                    <div class="border"></div>
+                    <div class="mask"></div>
+                </div>
+                {{closeBtn}}
             </div>
-            {{closeBtn}}
         </div>
     </div>
 </template>
@@ -70,72 +74,86 @@
     .yv-toast {
         position: fixed;
         z-index: 10;
-        background: $toast-bg;
         color: white;
-        padding: $toast-padding $big-padding;
-        max-width: $toast-width;
-        border-radius: $border-radius;
         opacity: .8;
-        display: flex;align-items: center;
-        box-shadow: 0 1px 3px $box-shadow-color;
-        overflow-x: hidden;
-
+        left:50%;
         &.position- {
             &center {
                 top: 50%;
-                left: 50%;
                 transform: translate(-50%, -50%);
-                animation: 1s fade-in-center linear;
             }
 
             &top {
-                top: 1em;
-                left: 50%;
+                top: 0;
                 transform: translateX(-50%);
-                animation: 1s fade-in-top linear;
             }
 
             &bottom {
-                bottom: 1em;
-                left: 50%;
+                bottom: 0;
                 transform: translateX(-50%);
-                animation: 1s fade-in-bottom linear;
             }
         }
+        > .wrapper {
+            padding: $toast-padding $big-padding;
+            max-width: $toast-width;
+            border-radius: $border-radius;
+            display: flex;align-items: center;
+            overflow-x: hidden;
+            background: $toast-bg;
+            box-shadow: 0 1px 3px $box-shadow-color;
+            &.position- {
+                &center {
+                    animation: 1s fade-in-center linear;
+                }
+
+                &top {
+                    animation: 1s fade-in-top linear;
+                    border-top-right-radius: 0;
+                    border-top-left-radius: 0;
+                }
+
+                &bottom {
+                    animation: 1s fade-in-bottom linear;
+                    border-bottom-right-radius: 0;
+                    border-bottom-left-radius: 0;
+                }
+            }
 
 
-        > .toast-close {
-            cursor: pointer;
-            padding-left: $big-padding;
-            margin-left: $big-padding;
-            flex-shrink: 0;
+            > .toast-close {
+                cursor: pointer;
+                padding-left: $big-padding;
+                margin-left: $big-padding;
+                flex-shrink: 0;
 
-            > .line {
-                position: absolute;
-                top: 0;
-                bottom: 0;
-                width: 100%;
-
-                > .border {
-                    width: 1px;
-                    background: #fff;
+                > .line {
                     position: absolute;
                     top: 0;
                     bottom: 0;
-                    left: calc(-.5px - #{$big-padding});
-                    opacity: .8;
-                }
+                    width: 100%;
 
-                > .mask {
-                    position: absolute;
-                    top: 0;
-                    bottom: 0;width: 100%;
-                    cursor: pointer;
-                    z-index: 3;
+                    > .border {
+                        width: 1px;
+                        background: #fff;
+                        position: absolute;
+                        top: 0;
+                        bottom: 0;
+                        left: calc(-.5px - #{$big-padding});
+                        opacity: .8;
+                    }
+
+                    > .mask {
+                        position: absolute;
+                        top: 0;
+                        bottom: 0;width: 100%;
+                        cursor: pointer;
+                        z-index: 3;
+                    }
                 }
             }
-        }
 
+        }
     }
+
 </style>
 
