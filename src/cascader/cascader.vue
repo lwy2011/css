5610416ff503f46e14ab1e4c9144c264129f8534
liuwanyong2017 <2674706698@qq.com>
2020-01-20 15:08:38 +0800

    初始化cascader组件

---
+++ src/cascader/cascader.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="yv-cascader">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style lang="scss" scoped>
+    .yv-cascader{
+
+    }
+</style>
