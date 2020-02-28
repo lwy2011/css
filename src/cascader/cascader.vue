448a3c0aad2700706d69ba4c31a1d1da02e75065
liuwanyong2017 <2674706698@qq.com>
2020-02-28 23:18:35 +0800

    实现点击外部隐藏弹窗

--- src/cascader/cascader.vue
+++ src/cascader/cascader.vue
@@ -1,10 +1,10 @@
 <template>
-    <div class="yv-cascader">
-        <div class="trigger" @click="visible=!visible">
+    <div class="yv-cascader" ref="cascader">
+        <div class="trigger" @click="trigger">
             <slot></slot>
-            {{result || '&nbsp;'}}
+            {{result || "&nbsp;"}}
         </div>
-        <div class="items" v-if="visible" >
+        <div class="items" v-if="visible">
             <cascaderItems
                     :ajax="ajax"
                     :selected="selected"
@@ -17,33 +17,63 @@
 
 <script>
     import cascaderItems from "./cascader-item.vue";
+
     export default {
         components: {
             cascaderItems
         },
         props: {
             selected: Array,
-            data:Array,
-            size:Object,
-            ajax:Function
+            data: Array,
+            size: Object,
+            ajax: Function
         },
-        data(){
+        data() {
             return {
-                visible:false
+                visible: false
+            };
+        },
+        methods: {
+            select($event) {
+                this.$emit("update:selected", $event);
+            },
+            documentClick(e) {
+                const {target} = e;
+                const {cascader} = this.$refs;
+                 // console.log(target,cascader);
+                if (target === cascader ||
+                    cascader.contains(target)) return;
+                this.close();
+            },
+            trigger() {
+                this.visible ? this.close() : this.open();
+            },
+            close() {
+                this.visible = false;
+                // console.log("close");
+                document.removeEventListener(
+                    "click", this.documentClick
+                );
+            },
+            open() {
+                this.visible = true;
+                this.$nextTick(
+                    () => {
+                        // console.log("open");
+                        document.addEventListener(
+                            "click", this.documentClick
+                        );
+                    }
+                );
             }
         },
-        methods:{
-            select($event){
-                this.$emit('update:selected',$event)
-            }
-        },
-        computed:{
-            result(){
+        computed: {
+            result() {
                 return this.selected.reduce(
-                    (a,b,ind)=> a + (
-                        b? (ind === 0 ? '':'-') + b.name :''
-                    ),''
-                )
+                    (a, b, ind) => a + (
+                        b ? (ind === 0 ? "" : "-") + b.name : ""
+                    ), ""
+                );
             }
         }
     };
@@ -51,24 +81,28 @@
 
 <style lang="scss" scoped>
     @import "../common";
+
     .yv-cascader {
-        position:relative;
-        >.trigger{
-            height:$input-height;
+        position: relative;
+        display: inline-block;
+
+        > .trigger {
+            height: $input-height;
             font-size: $font-size;
             border: 1px solid $border-color;
             border-radius: $border-radius;
             min-width: 9em;
-            padding:0 $small-padding ;
+            padding: 0 $small-padding;
             display: inline-flex;
             align-items: center;
             cursor: pointer;
         }
-        >.items{
-            position:absolute;
+
+        > .items {
+            position: absolute;
             @extend %box-shadow;
-            left:0;
-            top:calc(100% + 4px);
+            left: 0;
+            top: calc(100% + 4px);
         }
     }
 </style>
