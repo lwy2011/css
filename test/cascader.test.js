0e76f0b2f0ba7bb5e811bf1135dfb3668442d27a
liuwanyong2017 <2674706698@qq.com>
2020-03-04 18:45:18 +0800

    cascader测试用例有bug，暂时不写

---
+++ test/cascader.test.js
@@ -0,0 +1,10 @@
+// const expect = chai.expect;
+// //
+// // import Vue from "vue";
+// // // import iconHelper from "../docs/helper-icon.js";
+// // import Cascader from "../src/cascader/cascader.vue";
+// //
+// // Vue.config.devtools = false;
+// // Vue.config.productionTip = false;
+// //
+// // Vue.component("y-cascader", Cascader);
