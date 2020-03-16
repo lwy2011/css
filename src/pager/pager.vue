272c98f7f39ac806820a4e3512b0f155a4641c7e
liuwanyong2017 <2674706698@qq.com>
2020-03-16 18:39:48 +0800

    实现了点击切换的功能，key的值最好设置为数字，...会报错。

--- src/pager/pager.vue
+++ src/pager/pager.vue
@@ -1,24 +1,30 @@
 <template>
     <div class="yv-pager">
-        <span class="yv-pager-item last" :class="{disabled:current === 1}">
+        <span class="yv-pager-item last"
+              :class="{disabled:current === 1}"
+              @click="onLastClick"
+        >
             <y-icon icon="left"></y-icon>
         </span>
-        <template v-for="ind in pages">
+        <template v-for="(ind,index) in pages">
             <template v-if="ind !== '...'">
                 <span :class="{active:ind === current}" class="yv-pager-item"
-                      @click="onClickItem(ind)" :key="ind">
+                      @click="onClickItem(ind)" :key="index">
                     {{ind}}
                 </span>
             </template>
             <template v-else="ind === '...'">
                 <span class="yv-pager-item  yv-pager-dots"
-                      @click="onClickItem(ind)" :key="ind">
+                      @click="onClickItem(ind)" :key="index">
                     <y-icon icon="dots"></y-icon>
                 </span>
             </template>
         </template>
 
-        <span class="yv-pager-item  next" :class="{disabled:current === total}">
+        <span class="yv-pager-item  next"
+              :class="{disabled:current === total}"
+              @click="onNextClick"
+        >
             <y-icon icon="right"></y-icon>
         </span>
     </div>
@@ -74,12 +80,17 @@
             console.log(arr1);
 
             return {
-                pages: arr1
+                pages: this.updateView()
             };
         },
         computed: {
             currentUpdate() {
 
+            }
+        },
+        watch: {
+            current: function () {
+                this.pages = this.updateView();
             }
         },
         methods: {
@@ -89,6 +100,42 @@
                     this.$emit("update:current", val);
                 }
             },
+            onLastClick() {
+                if (this.current === 1) return;
+                this.$emit("update:current", this.current - 1);
+            },
+            onNextClick() {
+                if (this.current === this.total) return;
+                this.$emit("update:current", this.current + 1);
+            },
+            updateView() {
+                const {total, current} = this;
+                const arr = [1, current - 2, current - 1, current, current + 1, current + 2, total]
+                    .filter(val => val > 0).sort((a, b) => a - b);
+                const obj = {};
+                arr.map(
+                    val => obj["ind" + val] = val
+                );
+                let arr1 = Object.keys(obj).map(
+                    key => obj[key]
+                );
+                const maxIndex = arr1.indexOf(total);
+                if (maxIndex + 1 < arr1.length) {
+                    arr1 = arr1.slice(0, maxIndex + 1);
+                }
+                return arr1.reduce(
+                    (a, b) => {
+                        const last = a.length ? a[a.length - 1] : 0;
+                        if (last === "...") a.push(b);
+                        if (b - last > 1) {
+                            a = a.concat(["...", b]);
+                        } else {
+                            a.push(b);
+                        }
+                        return a;
+                    }, []
+                );
+            }
         }
     };
 </script>
