9c30f858080d7bc89b76b7f4f7877eab630195b6
liuwanyong2017 <2674706698@qq.com>
2020-04-17 13:40:34 +0800

    优化模拟加入file 触发change事件的代码。

--- test/upload.test.js
+++ test/upload.test.js
@@ -17,62 +17,79 @@ const html = () => `
             <button id="trigger">upload</button>
         </y-upload>
 `;
-const init = ()=>{
-    const div = document.createElement('div')
-    div.innerHTML = html()
-    document.body.appendChild(div)
+const init = () => {
+    const div = document.createElement("div");
+    div.innerHTML = html();
+    document.body.appendChild(div);
     const vm = new Vue({
-        el:div,
-        data:{
+        el: div,
+        data: {
             current: 1,
             action: "http://127.0.0.1:3000/image/upload",
             name: "avatar",
             ajaxCallback: res => {
                 console.log(res);
                 // const success = Math.random() > 0.5
-                const {url,code} = JSON.parse(res.response)
+                const {url, code} = JSON.parse(res.response);
                 return {
                     url,
-                    status:res.status,
+                    status: res.status,
                     errorMessage: code          //code
                 };
             },
-            files:[]
+            files: []
         },
         methods: {
-            onFail(val){
-                alert(val)
+            onFail(val) {
+                alert(val);
             }
         },
-        beforeDestroy(){
-            div.remove()
+        beforeDestroy() {
+            div.remove();
         }
-    })
+    });
     return vm;
-}
+};
+const addFiles = (input) => {
+    let file = new File(
+        [new ArrayBuffer(2e+5)],
+        "test-file.jpg",
+        {lastModified: null, type: "image/jpeg"})
+        , file1 = new File(
+        [new ArrayBuffer(2e+5)],
+        "test1-file.jpg",
+        {lastModified: null, type: "image/jpeg"});
+//https://stackoverflow.com/questions/52078853/is-it-possible-to-update-filelist/52079109
+    let list = new DataTransfer();
+    list.items.add(file);
+    list.items.add(file1);
+    input.files = list.files;
+    const evt = new Event("change");
+    input.dispatchEvent(evt);
+    return {file, file1};
+};
 describe(
     "Upload", () => {
         it("存在！", function () {
             expect(Upload).to.exist;
         });
-        it("可以上传图片", function () {
+        it("可以上传图片", function (done) {
             const vm = init();
-            const upload = vm.$children[0]
-            upload.$el.querySelector('#trigger').click()
+            const upload = vm.$children[0];
+            upload.$el.querySelector("#trigger").click();
             expect(upload.input).to.exist;
-            let file = new File(
-                [new ArrayBuffer(2e+5)],
-                'test-file.jpg',
-                { lastModified: null, type: 'image/jpeg' })
-                ,file1 = new File(
-                [new ArrayBuffer(2e+5)],
-                'test1-file.jpg',
-                { lastModified: null, type: 'image/jpeg' });
-//https://stackoverflow.com/questions/52078853/is-it-possible-to-update-filelist/52079109
-            let list = new DataTransfer();
-            list.items.add(file);
-            list.items.add(file1);
-            upload.input.files = list.files
+            const {input} = upload;
+            const {file, file1} = addFiles(input);
+            vm.$nextTick(
+                () => {
+                    const {files} = vm;
+                    expect(files[0].name).to.eq(file.name);
+                    expect(files[0].status).to.eq(1);
+                    expect(files[1].name).to.eq(file1.name);
+                    expect(files[1].status).to.eq(1);
+                    done();
+                }
+            );
         });
     }
 );
