5bdf263141e43c9b3e19c28c169b0e1df725bf31
liuwanyong2017 <2674706698@qq.com>
2020-09-12 19:05:36 +0800

    form  实例更新双向绑定的踩坑

--- docs/.vuepress/components/form-demo.vue
+++ docs/.vuepress/components/form-demo.vue
@@ -29,7 +29,7 @@
             回调函数里，可以拿到event，可以做很多事，不只是更新一个值！
         </p>
         <h4>记得在做table组件的时候，checkbox那个搞得我很难受！！现在再回顾一下！</h4>
-        <input type="checkbox" value="liu"  @change="onCheckboxChange">
+        <input type="checkbox" value="liu" @change="onCheckboxChange">
         <span>liu</span>
         <p>checked:{{checked}}</p>
         <p>
@@ -52,12 +52,11 @@
 
 
     export default {
-        components: {
-        },
+        components: {},
         data() {
             return {
-                str1:'test',
-                str2:'模拟v-model',
+                str1: "test",
+                str2: "模拟v-model",
                 content: `
 html:
     <div style="margin:1em;">
@@ -89,41 +88,62 @@ html:
             v-model的好处是省事，坏处是事件回调函数不能自定义，
             回调函数里，可以拿到event，可以做很多事，不只是更新一个值！
         </p>
+        <h4>记得在做table组件的时候，checkbox那个搞得我很难受！！现在再回顾一下！</h4>
+        <input type="checkbox" value="liu"  @change="onCheckboxChange">
+        <span>liu</span>
+        <p>checked:{{checked}}</p>
+        <p>
+            这次我是用了原生的事件思路做的。顺势而为！可能是上次的印象挺深的，，，
+            onchange事件，发生后，依靠元素的checked值，判断当前视图，从用户角度看，没问题。
+            得到用户看到的是否选上了。
+        </p>
+        <p>
+            上次钻牛角尖，对双向绑定比较模棱两可，认为用户点击了CheckBox，然后触发change事件，元素的checked跟数据绑定。
+            我操作checked，进而操作视图。忽略了原生事件的因果关系！现在想来，是先checked变了，才会发生change事件。这就是顺势，之前我想
+            的是大大的控制欲，逆势，逆了原生事件。
+        </p>
         <pre>
             <code>{{content}}</code>
         </pre>
     </div>
+
  js :
         data() {
             return {
                 str1:'test',
                 str2:'模拟v-model',
-
-            };
-        },
-        methods: {
-            onChange(e){
-                this.str2 = e.target.value
-            }
-        }
-                `,
                 checked:null,
             };
-        },
-        computed:{
-            value(){
-                return this.checked ? 'yes':'no'
-            }
         },
         methods: {
             onChange(e){
                 this.str2 = e.target.value
             },
-            onCheckboxChange(e){
+             onCheckboxChange(e){
                 const {value,checked} = e.target
 
                 console.log(value,checked);
                 this.checked = checked ? value : null
+            }
+        }
+                `,
+                checked: null,
+            };
+        },
+        computed: {
+            value() {
+                return this.checked ? "yes" : "no";
+            }
+        },
+        methods: {
+            onChange(e) {
+                this.str2 = e.target.value;
+            },
+            onCheckboxChange(e) {
+                const {value, checked} = e.target;
+
+                console.log(value, checked);
+                this.checked = checked ? value : null;
             }
         }
     };
