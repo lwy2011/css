51c8737cc07eabd1e8f60994a2fcf32cf6dda723
liuwanyong2017 <2674706698@qq.com>
2020-09-13 12:28:10 +0800

    完善组件通讯，props,$emit,自定义事件 实例实现

--- docs/.vuepress/components/component-demo.vue
+++ docs/.vuepress/components/component-demo.vue
@@ -1,13 +1,15 @@
 <template>
     <div style="margin:1em;">
-        <h3>component</h3>
+        <h3>component </h3>
         <h4>组件通讯</h4>
         <p>props，$emit ,自定义事件</p>
         <div>
+            <p>自定义事件对组件属性的修改，以及视图的更新test:{{event}}</p>
             <p>
                 <input type="text" v-model="message">
                 <button @click="add">add</button>
             </p>
+
             <ul>
                 <List v-for="(item,index) in lists"
                       :data="item"
@@ -29,11 +31,11 @@
             <h4>List Component code</h4>
             <img src="./component/list.png" alt="image">
         </div>
-       <p>
-           自定义事件相当于一个window的人设，公共平台，可以接收和分发各种信息！不论嵌套多少层级。
-           props,$emit是最基本父子组件通信的手段，当层级深了，可以用自定义事件来进行父子组件间的通讯。
-           如果自定义事件的event只关注于数据层面，自身的data里面进行数据存储，其实就是单向数据流的状态管理了。
-       </p>
+        <p>
+            自定义事件相当于一个window的人设，公共平台，可以接收和分发各种信息！不论嵌套多少层级。
+            props,$emit是最基本父子组件通信的手段，当层级深了，可以用自定义事件来进行父子组件间的通讯。
+            如果自定义事件的event只关注于数据层面，自身的data里面进行数据存储，其实就是单向数据流的状态管理了。
+        </p>
         <pre>
             <code>{{content}}</code>
         </pre>
@@ -59,6 +61,7 @@ html:
         <h4>组件通讯</h4>
         <p>props，$emit ,自定义事件</p>
         <div>
+            <p>自定义事件对组件属性的修改，以及视图的更新test:{{event}}</p>
             <p>
                 <input type="text" v-model="message">
                 <button @click="add">add</button>
@@ -102,15 +105,16 @@ js:
                     {message: "title", id: 1}
                 ],
                 count: 2,
+                event:null
             };
         },
         mounted() {
             this.onDeleteCallback = (index) => {
-                alert("用自定义事件，进行组件间的消息通信：onDelete index=" + \${index});
+                this.event = "用自定义事件，进行组件间的消息通信：onDelete index=" + \${index};
             }
             event.$on("onDelete", this.onDeleteCallback);
             this.onAddCallback = (message) => {
-                alert("用自定义事件，进行组件间的消息通信：onAdd message=" + \${message}\);
+                this.event = "用自定义事件，进行组件间的消息通信：onAdd message=" + \${message}\
             }
             event.$on("onAdd",this.onAddCallback );
         },
@@ -142,15 +146,16 @@ js:
                     {message: "title", id: 1}
                 ],
                 count: 2,
+                event: null,
             };
         },
         mounted() {
             this.onDeleteCallback = (index) => {
-                alert("用自定义事件，进行组件间的消息通信：onDelete index=" + `${index}`);
+                this.event = "用自定义事件，进行组件间的消息通信：onDelete index=" + `${index}`;
             };
             event.$on("onDelete", this.onDeleteCallback);
             this.onAddCallback = (message) => {
-                alert("用自定义事件，进行组件间的消息通信：onAdd message=" + `${message}`);
+                this.event = "用自定义事件，进行组件间的消息通信：onAdd message=" + `${message}`;
             };
             event.$on("onAdd", this.onAddCallback);
         },
